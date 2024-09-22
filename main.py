d6f9448057f182e2c4d955bed54afdbd4f5cade9
Chaitanya Lohani <109726533+AloneFFOfficial@users.noreply.github.com>
2024-09-22 05:43:33 +0000

    a few changes in main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import sqlite3
+import sqlite3 as sql
+
+class Database:
+    def __init__(self,table_name):
+        conn = sql.connect('Data.db')
+        cur = conn.cursur()
+        data = cur.execute(f'select * from {table_name}')
+        self.data = data.fetchall()
+
+    def __str__(self):
+        return self.data
+    
 
 
+class query(Database):
+    def __init__(self):
+        super().__init__()
