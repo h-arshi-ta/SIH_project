0948ab1e8a2dbfb72b8a13aa3fca1654edd0b7e0
h-arshi-ta <159037215+h-arshi-ta@users.noreply.github.com>
2024-09-22 12:08:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3 as sql
 class Database:
     def __init__(self,table_name):
         conn = sql.connect('Data.db')
-        cur = conn.cursur()
+        cur = conn.cursor()
         data = cur.execute(f'select * from {table_name}')
         self.data = data.fetchall()
 
@@ -15,3 +15,4 @@ class Database:
 class query(Database):
     def __init__(self):
         super().__init__()
+        
